843b4ac13df1369205e2d36029a78c6d6f92ef57
JasonO <JasonO@264a4808-452e-4e3f-8587-5e799483ef68>
2010-10-12 03:58:48 +0000

    Adding ReleaseVersion entity and data access classes. Adding fake repository for TestCases.

--- trunk/Arena.Custom.RefreshCache.QAChecklist.Tests/Fakes/FakeTestCaseRepository.cs
+++ trunk/Arena.Custom.RefreshCache.QAChecklist.Tests/Fakes/FakeTestCaseRepository.cs
@@ -9,14 +9,62 @@
 * 
 * $Log: $
 **********************************************************************/
-using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Arena.Custom.RefreshCache.QAChecklist.Data;
+using Arena.Custom.RefreshCache.QAChecklist.Entities;
 
 
 namespace Arena.Custom.RefreshCache.QAChecklist.Tests
 {
-	[TestFixture]
-	public class TestCaseTests
+	public class FakeTestCaseRepository : ITestCaseRepository
 	{
-		
+	    private readonly IList<TestCase> testCases;
+
+        public FakeTestCaseRepository()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                testCases.Add(new TestCase
+                                  {
+                                      TestCaseID = i + 1,
+                                      Category = 0,
+                                      Description = string.Empty,
+                                      Name = string.Empty
+                                  });
+            }
+        }
+
+	    public IEnumerable<TestCase> GetAllTestCases()
+	    {
+	        return testCases.ToList();
+	    }
+
+	    public IEnumerable<TestCase> GetTestCasesByCategory(int categoryID)
+	    {
+	        return testCases.Where(tc => tc.Category == categoryID);
+	    }
+
+	    public TestCase GetTestCase(int id)
+	    {
+	        return testCases.FirstOrDefault(tc => tc.TestCaseID == id);
+	    }
+
+	    public void Save()
+	    {
+	    }
+
+	    public void Delete(TestCase testCase)
+	    {
+	        if (testCases.Any(tc => tc.TestCaseID == testCase.TestCaseID))
+	        {
+	            testCases.Remove(testCase);
+	        }
+	    }
+
+	    public bool Exists(int id)
+	    {
+	        return testCases.Any(tc => tc.TestCaseID == id);
+	    }
 	}
 }
